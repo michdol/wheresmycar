0a6370f153583113d18c0a3d79fa03fb71436f17
michdol <michal.dolbniak@gmail.com>
2024-06-25 08:51:36 +0200

    First commit

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM python:3.12-slim-bullseye AS build
+
+ARG PROJECT_NAME=wheresmycar
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+RUN apt-get update && \
+    apt-get install --no-install-recommends -y \
+        libpython3-dev \
+        gcc && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN pip install --upgrade pip
+
+CMD mkdir ${PROJECT_NAME}
+COPY . ${PROJECT_NAME}
+WORKDIR ${PROJECT_NAME}
+
+RUN pip install --no-cache-dir --upgrade -r ${PROJECT_NAME}/requirements.txt
+
+ENV PYTHONUNBUFFERED 1
+ENV PYTHONDONTWRITEBYTECODE 1
+CMD tail -f /dev/null
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'Image detection with PyTorch'
+LONG_DESCRIPTION = 'Packge for detecting cars and number plates on images'
+
+setup(
+        name="wheresmycar", 
+        version=VERSION,
+        author="Michal Dolbniak",
+        author_email="md69626@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[
+            'torch',
+            'torchvision',
+        ],
+        keywords=['python', 'pytorch']
+)
