42c64d6504007718f5d4305a0a8115fe6bfd447d
michdol <michal.dolbniak@gmail.com>
2024-06-27 14:32:42 +0200

    PlateDetector class

--- wheresmycar/plate_detector.py
+++ wheresmycar/plate_detector.py
@@ -1,33 +1,65 @@
-from os import path
+"""plate_detector module"""
+import os
 import torch
 from ultralytics import YOLO
+from ultralytics.engine.results import Results
 
 
-def load_model(device: str):
-    model = YOLO("/wheresmycar/model_training/runs/detect/train5/weights/best.pt")
-    model.to(device)
-    return model
+class PlateDetector():
+    """Class for Vehicle Number Plate Detection based on pretrained YOLOv8 model.
+    """
+    DEVICE_CPU = "cpu"
+    DEVICE_CUDA = "cuda"
 
+    def __init__(self, enable_cuda=True):
+        self.device = self.get_device(enable_cuda)
+        self._model = self.load_model(self.device)
 
-def main():
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model = load_model(device)
+    def load_model(self, device: str) -> YOLO:
+        """
+        Function to load pretrained model.
+        params:
+        - device <str>: device on which the model should run
+        returns:
+        - <ultralytics.YOLO>: pretrained YOLOv8 model
+        """
+        weights_path: str = self._get_weights_path()
+        model = YOLO(weights_path)
+        model.to(device)
+        return model
 
-    img_path = "/wheresmycar/model_training/data/images/val/images/"
-    # img_path = "/wheresmycar/model_training/data/images/val/images/Cars417_jpg.rf.3ba955c4339fa1f5a957cca648f2d464.jpg"
-    output = model.predict(img_path, conf=0.5)
+    def _get_weights_path(self) -> str:
+        dirname = os.path.dirname(__file__)
+        return os.path.join(dirname, "weights/best.pt")
 
-    # print("Results", output)
-    file_name = path.basename(img_path)
-    # print(output)
-    for r in output:
-        # print(r)
-        # print("boxes")
-        # print(r.boxes.data.tolist())
-        if len(r.boxes) > 0:
-            f_name = path.basename(r.path)
-            r.save_crop("./output/{}".format(f_name))
+    @property
+    def model(self) -> YOLO:
+        """
+        Access pretrained model
+        returns:
+        - <ultralytics.YOLO>: pretrained YOLOv8 model
+        """
+        return self._model
 
+    def get_device(self, enable_cuda: bool) -> str:
+        """
+        Gets target device for inference.
+        params:
+        - enable_cude <bool>: if True, will return cuda if available
+        returns:
+        - <str> either cuda or cpu
+        """
+        if enable_cuda:
+            return self.DEVICE_CUDA if torch.cuda.is_available() else self.DEVICE_CPU
+        return self.DEVICE_CPU
 
-if __name__ == "__main__":
-    main()
+    def detect(self, target_path: str, conf: float = 0.5, **kwargs) -> Results:
+        """
+        Get predictions on given input.
+        params:
+        - target_path <str>: path to directory with images or image's file path
+        - conf <float>: minimum confidence threshold for detection
+        returns:
+        - <ultralytics.engine.results.Results>: inference results
+        """
+        return self._model.predict(target_path, conf=conf, **kwargs)
