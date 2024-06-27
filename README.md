44218be4a6c8c97e4404c3fbbdb8a4ee6f128a75
michdol <michal.dolbniak@gmail.com>
2024-06-27 14:36:38 +0200

    Clean up

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # wheresmycar
+
+First attempt at training and utilizing YOLOv8 model for vehicle number plate detection.
--- model_training/main.py
+++ model_training/main.py
@@ -1,6 +1,15 @@
+"""Script for training the model."""
 from ultralytics import YOLO
 
-# model = YOLO("yolov8n.yaml")
-model = YOLO("runs/detect/train5/weights/best.pt")
 
-results = model.train(data="config.yaml", epochs=20)
+def main():
+    """main function
+    """
+    # Original model
+    # model = YOLO("yolov8n.yaml")
+    model = YOLO("runs/detect/train5/weights/best.pt")
+    model.train(data="config.yaml", epochs=20)
+
+
+if __name__ == "__main__":
+    main()
